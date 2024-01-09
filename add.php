3b980612bafb1f18bba0d1d3ec85700762f2ea1d
Kevin <kevinlai29@outlook.com>
2024-01-09 15:24:04 +0800

    0109-02

--- add.php
+++ add.php
@@ -8,7 +8,7 @@ $title = '新增';
 <?php include __DIR__ . '/parts/navbar.php' ?>
 <style>
   form .mb-3 .form-text {
-    color:red;
+    color: red;
   }
 </style>
 <div class="container">
@@ -52,69 +52,35 @@ $title = '新增';
       </div>
     </div>
   </div>
+  <!-- Button trigger modal -->
+  <!--
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Launch demo modal
+  </button>
+-->
+</div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">新增結果</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="alert alert-success" role="alert">
+          新增成功
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">繼續新增</button>
+        <a type="button" class="btn btn-primary" href="list.php">到列表頁</a>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php include __DIR__ . '/parts/scripts.php' ?>
-<!-- <script>
-  const {
-    name: name_f,
-    email: email_f,
-    mobil: mobile_f
-  } = document.form1;
-  function validateEmail(email){
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  }
-  function validateMobile(mobile){
-    var re = /^09\d{2}-?\{3}-?\d{3}$/;
-    return re.test(mobile);
-  }
-
-
-  const sendForm = e => {
-    e.preventDefault();
-    name_f.style.border = '1px solid #CCC';
-    name_f.nextElementSibling.innerHTML = "";
-    email_f.style.border = '1px solid #CCC';
-    email_f.nextElementSibling.innerHTML = "";
-    mobile_f.style.border = '1px solid #CCC';
-    mobile_f.nextElementSibling.innerHTML = "";
-
-  let isPass = true;
-  if(name_f.value.length<2){
-    // alert("please enter name");
-    isPass= false;
-    name_f.style.border = '1px solid red';
-    name_f.nextElementSibling.innerHTML = "請填入正確姓名";
-  }
-  if(email_f.value && !validateEmail(email_f.value)){
-    
-    isPass= false;
-    email_f.style.border = '1px solid red';
-    email_f.nextElementSibling.innerHTML = "請填入正確email";
-  }
-  if(mobile.value && !validateMobile(mobile.value)){
-    
-    isPass= false;
-    mobile_f.style.border = '1px solid red';
-    mobile_f.nextElementSibling.innerHTML = "請填入正確電話";
-  }
-  if (isPass) {
-    const fd = new FormData(document.form1);
-    fetch('add-api.php', {
-        method: 'POST',
-        body: fd, //content-type:multipart/form-data
-      }).then(r => r.json())
-      .then(result => {
-        console.log({
-          result
-        });
-      })
-      .catch(ex => console.log(ex))
-
-  }
-  }
-</script> -->
 <script>
   const {
     name: name_f,
@@ -178,14 +144,16 @@ $title = '新增';
           console.log({
             result
           });
+          if (result.success) {
+            myModal.show();
+          }
         })
         .catch(ex => console.log(ex))
     }
 
 
   }
+  const myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
 </script>
 
 <?php include __DIR__ . '/parts/html-foot.php' ?>
-
-
